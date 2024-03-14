f83e7206ee6e8075db4586b7eed11555eacff71c
Vishnu Mevada <vishnu.m@samcomtechnobrains.com>
2024-03-14 12:19:59 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
+
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
-	// Add custom route here if needed
-	jsonServer.rewriter({
-		"/api/*": "/$1",
-	})
+    // Add custom route here if needed
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+    })
 );
 server.use(router);
 server.listen(3000, () => {
-	console.log("JSON Server is running");
+    console.log("JSON Server is running");
 });
 
 // Export the Server API
