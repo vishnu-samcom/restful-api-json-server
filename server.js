c2ab1877a9dd5da807be68697c88d260dbf8b2f3
Vishnu Mevada <vishnu.m@samcomtechnobrains.com>
2024-03-14 12:31:46 +0530

    fixed bugs

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // JSON Server module
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
 
 server.use(middlewares);
+
 // Add this before server.use(router)
 server.use(
     // Add custom route here if needed
@@ -14,9 +17,13 @@ server.use(
         "/api/*": "/$1",
     })
 );
+
 server.use(router);
-server.listen(3000, () => {
-    console.log("JSON Server is running");
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
 });
 
 // Export the Server API
